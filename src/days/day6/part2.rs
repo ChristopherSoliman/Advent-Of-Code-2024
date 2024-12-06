3a6a6643613f8c36c6f2f0f5b651fc42bbf21937
ChristopherSoliman <csoliman10@gmail.com>
2024-12-06 13:03:48 +0200

    day 6 part 2 WIP

--- src/days/day6/part2.rs
+++ src/days/day6/part2.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 enum Direction {
     Up,
     Down,
@@ -15,7 +18,7 @@ impl Direction {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
 struct Position {
     pub x: isize,
     pub y: isize,
@@ -39,9 +42,15 @@ pub fn part2(path: &str) -> u32 {
         })
         .collect();
 
-    let visited = vec![false; map[0].len()];
-    let mut visited = vec![visited; map.len()];
-    visited[pos.y as usize][pos.x as usize] = true;
+    let initial_pos = pos.clone();
+
+    let mut visited_directions: HashMap<Position, Vec<Direction>> = HashMap::new();
+    visited_directions.insert(pos, vec![Direction::Up]);
+
+    let mut count = 0;
+
+    let width = map[0].len() as isize;
+    let height = map.len() as isize;
 
     loop {
         let starting_pos = pos.clone();
@@ -51,16 +60,63 @@ pub fn part2(path: &str) -> u32 {
             Direction::Left => pos.x -= 1,
             Direction::Right => pos.x += 1,
         }
-        if pos.x as usize >= map[0].len() || pos.y as usize >= map.len() || pos.x < 0 || pos.y < 0 {
+        if pos.x >= width || pos.y >= height || pos.x < 0 || pos.y < 0 {
             break;
         }
         if map[pos.y as usize][pos.x as usize] == '#' {
             dir = dir.get_next_direction();
             pos = starting_pos;
         } else {
-            visited[pos.y as usize][pos.x as usize] = true;
+            if causes_loop(&pos, &dir.get_next_direction(), &map, &visited_directions) {
+                count += 1;
+            }
+            match visited_directions.get_mut(&pos) {
+                Some(d) => {
+                    if !d.contains(&dir) {
+                        d.push(dir);
+                    }
+                }
+                None => {
+                    visited_directions.insert(pos, vec![dir]);
+                }
+            }
         }
     }
 
-    todo!();
+    count
+}
+
+fn causes_loop(
+    position: &Position,
+    dir: &Direction,
+    map: &Vec<Vec<char>>,
+    visited_directions: &HashMap<Position, Vec<Direction>>,
+) -> bool {
+    let mut pos = position.clone();
+
+    let width = map[0].len() as isize;
+    let height = map.len() as isize;
+
+    loop {
+        match dir {
+            Direction::Up => pos.y -= 1,
+            Direction::Down => pos.y += 1,
+            Direction::Left => pos.x -= 1,
+            Direction::Right => pos.x += 1,
+        }
+
+        if pos.x >= width
+            || pos.y >= height
+            || pos.x < 0
+            || pos.y < 0
+            || map[pos.y as usize][pos.x as usize] == '#'
+        {
+            return false;
+        }
+        if let Some(c) = visited_directions.get(&pos) {
+            if c.contains(&dir) {
+                return true;
+            }
+        }
+    }
 }
