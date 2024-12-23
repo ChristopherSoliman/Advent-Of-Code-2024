c891291a4be249e2cb978ff70cd257962eec9c4a
ChristopherSoliman <csoliman10@gmail.com>
2024-12-23 14:45:05 +0200

    day 21: removed duplicated code

--- src/days/day21/part1.rs
+++ src/days/day21/part1.rs
@@ -2,22 +2,26 @@ use std::collections::{HashMap, HashSet};
 
 const DIRECTIONS: [(i8, i8); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 
-const NUMPAD: [[char; 3]; 4] = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-    [' ', '0', 'A'],
-];
-const ARROWS: [[char; 3]; 2] = [[' ', '^', 'A'], ['<', 'v', '>']];
-
 pub fn part1(path: &str) -> u32 {
     let input = std::fs::read_to_string(path).expect("File should be there");
-    let mut sum = 0;
-    for line in input.lines() {
-        let num = line.strip_suffix("A").unwrap().parse::<u32>().unwrap();
-        sum += num * get_code_length(&line);
-    }
-    sum
+    let numpad = vec![
+        vec!['7', '8', '9'],
+        vec!['4', '5', '6'],
+        vec!['1', '2', '3'],
+        vec![' ', '0', 'A'],
+    ];
+    let arrows = vec![vec![' ', '^', 'A'], vec!['<', 'v', '>']];
+    input
+        .lines()
+        .map(|line| {
+            (
+                line,
+                line.strip_suffix("A").unwrap().parse::<u32>().unwrap(),
+            )
+        })
+        .fold(0, |acc, (line, num)| {
+            acc + num * get_lowest_score(&line, 3, true, &numpad, &arrows)
+        })
 }
 
 fn map_dir(dir: &(i8, i8)) -> char {
@@ -29,70 +33,22 @@ fn map_dir(dir: &(i8, i8)) -> char {
         _ => unreachable!(),
     }
 }
-
-fn get_code_length(code: &str) -> u32 {
-    let mut result = 0;
-    let code = "A".to_string() + code;
-    for i in 0..code.len() - 1 {
-        let mut code_it = code.chars();
-        result += get_shortest(&code_it.nth(i).unwrap(), &code_it.next().unwrap());
-    }
-    result
-}
-
-fn get_shortest(num1: &char, num2: &char) -> u32 {
-    let mut seen: HashSet<char> = HashSet::new();
-    let mut q: Vec<(char, String)> = Vec::from([(*num1, "".to_string())]);
-    let mut paths: HashMap<char, Vec<String>> = HashMap::new();
-
-    paths.insert(*num1, vec![]);
-
-    while !q.is_empty() {
-        let (c, path) = q.remove(0);
-        seen.insert(c);
-        let mut c_pos = (0, 0);
-        NUMPAD.iter().enumerate().for_each(|(i, r)| {
-            if let Some(j) = r.iter().position(|x| *x == c) {
-                c_pos = (i, j);
-            }
-        });
-        for dir in DIRECTIONS {
-            let (nr, nc) = (c_pos.0 as i32 + dir.0 as i32, c_pos.1 as i32 + dir.1 as i32);
-            if nr < 0 || nr >= 4 || nc < 0 || nc >= 3 || NUMPAD[nr as usize][nc as usize] == ' ' {
-                continue;
-            }
-            let new_c = NUMPAD[nr as usize][nc as usize];
-            if seen.contains(&new_c) {
-                continue;
-            }
-
-            let empty = Vec::new();
-            let old_paths = paths.get(&new_c).or(Some(&empty)).unwrap();
-            let new_path = path.clone() + &map_dir(&dir).to_string();
-            if old_paths.len() == 0 || old_paths[0].len() > new_path.len() {
-                paths.insert(new_c, vec![new_path.clone()]);
-            } else if old_paths[0].len() < new_path.len() {
-                continue;
-            } else {
-                paths.entry(new_c).and_modify(|v| v.push(new_path.clone()));
-            }
-            q.push((new_c, new_path));
-        }
-        q.sort_by_key(|v| v.1.len());
-    }
-
-    paths
-        .get(&num2)
-        .unwrap()
-        .iter()
-        .map(|v| get_lowest_score(v.to_owned() + "A", 2))
-        .min()
-        .unwrap()
-}
-
-fn get_lowest_score(code: String, level: u8) -> u32 {
+fn get_lowest_score(
+    code: &str,
+    level: u8,
+    first: bool,
+    numpad: &Vec<Vec<char>>,
+    arrows: &Vec<Vec<char>>,
+) -> u32 {
     let mut result = 0;
     let code = "A".to_string() + &code;
+
+    let keypad = match first {
+        true => numpad,
+        false => arrows,
+    };
+
+    let height = keypad.len();
 
     for i in 0..code.len() - 1 {
         let mut code_it = code.chars();
@@ -102,6 +58,7 @@ fn get_lowest_score(code: String, level: u8) -> u32 {
             result += 1;
             continue;
         }
+
         let mut seen: HashSet<char> = HashSet::new();
         let mut q: Vec<(char, String)> = Vec::from([(*char1, "".to_string())]);
         let mut paths: HashMap<char, Vec<String>> = HashMap::new();
@@ -112,18 +69,22 @@ fn get_lowest_score(code: String, level: u8) -> u32 {
             let (c, path) = q.remove(0);
             seen.insert(c);
             let mut c_pos = (0, 0);
-            ARROWS.iter().enumerate().for_each(|(i, r)| {
+            keypad.iter().enumerate().for_each(|(i, r)| {
                 if let Some(j) = r.iter().position(|x| *x == c) {
                     c_pos = (i, j);
                 }
             });
             for dir in DIRECTIONS {
                 let (nr, nc) = (c_pos.0 as i32 + dir.0 as i32, c_pos.1 as i32 + dir.1 as i32);
-                if nr < 0 || nr >= 2 || nc < 0 || nc >= 3 || ARROWS[nr as usize][nc as usize] == ' '
+                if nr < 0
+                    || nr >= height as i32
+                    || nc < 0
+                    || nc >= 3
+                    || keypad[nr as usize][nc as usize] == ' '
                 {
                     continue;
                 }
-                let new_c = ARROWS[nr as usize][nc as usize];
+                let new_c = keypad[nr as usize][nc as usize];
                 if seen.contains(&new_c) {
                     continue;
                 }
@@ -140,23 +101,21 @@ fn get_lowest_score(code: String, level: u8) -> u32 {
                 }
                 q.push((new_c, new_path));
             }
-            q.sort_by_key(|v| v.1.len());
         }
 
-        if level == 1 {
-            if let Some(path) = paths.get(&char2) {
-                result += path[0].len() as u32 + 1;
-            }
-        } else {
-            result += paths
-                .get(&char2)
-                .unwrap()
-                .iter()
-                .map(|v| get_lowest_score(v.to_owned() + "A", level - 1))
-                .min()
-                .or(Some(0))
-                .unwrap();
-        }
+        result += paths
+            .get(&char2)
+            .unwrap()
+            .iter()
+            .map(|v| {
+                if level == 1 {
+                    return v.len() as u32 + 1;
+                }
+                get_lowest_score(&(v.to_owned() + "A")[..], level - 1, false, numpad, arrows)
+            })
+            .min()
+            .or(Some(0))
+            .unwrap();
     }
 
     result
--- src/days/day21/part2.rs
+++ src/days/day21/part2.rs
@@ -2,13 +2,29 @@ use std::collections::{HashMap, HashSet};
 
 const DIRECTIONS: [(i8, i8); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 
-const NUMPAD: [[char; 3]; 4] = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-    [' ', '0', 'A'],
-];
-const ARROWS: [[char; 3]; 2] = [[' ', '^', 'A'], ['<', 'v', '>']];
+pub fn part2(path: &str) -> u64 {
+    let input = std::fs::read_to_string(path).expect("File should be there");
+    let numpad = vec![
+        vec!['7', '8', '9'],
+        vec!['4', '5', '6'],
+        vec!['1', '2', '3'],
+        vec![' ', '0', 'A'],
+    ];
+    let arrows = vec![vec![' ', '^', 'A'], vec!['<', 'v', '>']];
+    let mut cache: HashMap<(char, char, u8), u64> = HashMap::new();
+
+    input
+        .lines()
+        .map(|line| {
+            (
+                line,
+                line.strip_suffix("A").unwrap().parse::<u64>().unwrap(),
+            )
+        })
+        .fold(0, |acc, (line, num)| {
+            acc + num * get_lowest_score(&line, 26, true, &numpad, &arrows, &mut cache)
+        })
+}
 
 fn map_dir(dir: &(i8, i8)) -> char {
     match dir {
@@ -19,85 +35,29 @@ fn map_dir(dir: &(i8, i8)) -> char {
         _ => unreachable!(),
     }
 }
-
-pub fn part2(path: &str) -> u64 {
-    let input = std::fs::read_to_string(path).expect("File should be there");
-    let mut sum = 0;
-    let mut cache: HashMap<(char, char, u8), u64> = HashMap::new();
-    for line in input.lines() {
-        let num = line.strip_suffix("A").unwrap().parse::<u64>().unwrap();
-        sum += num * get_shortest(&line, &mut cache);
-    }
-    sum
-}
-
-fn get_shortest(code: &str, cache: &mut HashMap<(char, char, u8), u64>) -> u64 {
-    let mut result = 0;
-    let code = "A".to_string() + code;
-    for i in 0..code.len() - 1 {
-        let mut code_it = code.chars();
-        let s = &code_it.nth(i).unwrap();
-        let t = &code_it.next().unwrap();
-
-        let mut seen: HashSet<char> = HashSet::new();
-        let mut q: Vec<(char, String)> = Vec::from([(*s, "".to_string())]);
-        let mut paths: HashMap<char, Vec<String>> = HashMap::new();
-
-        paths.insert(*s, vec![]);
-
-        while !q.is_empty() {
-            let (c, path) = q.remove(0);
-            seen.insert(c);
-            let mut c_pos = (0, 0);
-            NUMPAD.iter().enumerate().for_each(|(i, r)| {
-                if let Some(j) = r.iter().position(|x| *x == c) {
-                    c_pos = (i, j);
-                }
-            });
-            for dir in DIRECTIONS {
-                let (nr, nc) = (c_pos.0 as i32 + dir.0 as i32, c_pos.1 as i32 + dir.1 as i32);
-                if nr < 0 || nr >= 4 || nc < 0 || nc >= 3 || NUMPAD[nr as usize][nc as usize] == ' '
-                {
-                    continue;
-                }
-                let new_c = NUMPAD[nr as usize][nc as usize];
-                if seen.contains(&new_c) {
-                    continue;
-                }
-
-                let empty = Vec::new();
-                let old_paths = paths.get(&new_c).or(Some(&empty)).unwrap();
-                let new_path = path.clone() + &map_dir(&dir).to_string();
-                if old_paths.len() == 0 || old_paths[0].len() > new_path.len() {
-                    paths.insert(new_c, vec![new_path.clone()]);
-                } else if old_paths[0].len() < new_path.len() {
-                    continue;
-                } else {
-                    paths.entry(new_c).and_modify(|v| v.push(new_path.clone()));
-                }
-                q.push((new_c, new_path));
-            }
-            q.sort_by_key(|v| v.1.len());
-        }
-        result += paths
-            .get(&t)
-            .unwrap()
-            .iter()
-            .map(|v| get_lowest_score(v.to_owned() + "A", cache, 25))
-            .min()
-            .unwrap()
-    }
-    result
-}
-
-fn get_lowest_score(code: String, cache: &mut HashMap<(char, char, u8), u64>, level: u8) -> u64 {
+fn get_lowest_score(
+    code: &str,
+    level: u8,
+    first: bool,
+    numpad: &Vec<Vec<char>>,
+    arrows: &Vec<Vec<char>>,
+    cache: &mut HashMap<(char, char, u8), u64>,
+) -> u64 {
     let mut result = 0;
     let code = "A".to_string() + &code;
+
+    let keypad = match first {
+        true => numpad,
+        false => arrows,
+    };
+
+    let height = keypad.len();
 
     for i in 0..code.len() - 1 {
         let mut code_it = code.chars();
         let char1 = &code_it.nth(i).unwrap();
         let char2 = &code_it.next().unwrap();
+
         if let Some(res) = cache.get(&(*char1, *char2, level)) {
             result += res;
             continue;
@@ -106,6 +66,7 @@ fn get_lowest_score(code: String, cache: &mut HashMap<(char, char, u8), u64>, le
             result += 1;
             continue;
         }
+
         let mut seen: HashSet<char> = HashSet::new();
         let mut q: Vec<(char, String)> = Vec::from([(*char1, "".to_string())]);
         let mut paths: HashMap<char, Vec<String>> = HashMap::new();
@@ -116,18 +77,22 @@ fn get_lowest_score(code: String, cache: &mut HashMap<(char, char, u8), u64>, le
             let (c, path) = q.remove(0);
             seen.insert(c);
             let mut c_pos = (0, 0);
-            ARROWS.iter().enumerate().for_each(|(i, r)| {
+            keypad.iter().enumerate().for_each(|(i, r)| {
                 if let Some(j) = r.iter().position(|x| *x == c) {
                     c_pos = (i, j);
                 }
             });
             for dir in DIRECTIONS {
                 let (nr, nc) = (c_pos.0 as i32 + dir.0 as i32, c_pos.1 as i32 + dir.1 as i32);
-                if nr < 0 || nr >= 2 || nc < 0 || nc >= 3 || ARROWS[nr as usize][nc as usize] == ' '
+                if nr < 0
+                    || nr >= height as i32
+                    || nc < 0
+                    || nc >= 3
+                    || keypad[nr as usize][nc as usize] == ' '
                 {
                     continue;
                 }
-                let new_c = ARROWS[nr as usize][nc as usize];
+                let new_c = keypad[nr as usize][nc as usize];
                 if seen.contains(&new_c) {
                     continue;
                 }
@@ -144,26 +109,30 @@ fn get_lowest_score(code: String, cache: &mut HashMap<(char, char, u8), u64>, le
                 }
                 q.push((new_c, new_path));
             }
-            q.sort_by_key(|v| v.1.len());
         }
 
-        if level == 1 {
-            if let Some(path) = paths.get(&char2) {
-                result += path[0].len() as u64 + 1;
-                cache.insert((*char1, *char2, level), path[0].len() as u64 + 1);
-            }
-        } else {
-            let min = paths
-                .get(&char2)
-                .unwrap()
-                .iter()
-                .map(|v| get_lowest_score(v.to_owned() + "A", cache, level - 1))
-                .min()
-                .or(Some(0))
-                .unwrap();
-            cache.insert((*char1, *char2, level), min as u64);
-            result += min;
-        }
+        let min = paths
+            .get(&char2)
+            .unwrap()
+            .iter()
+            .map(|v| {
+                if level == 1 {
+                    return v.len() as u64 + 1;
+                }
+                get_lowest_score(
+                    &(v.to_owned() + "A")[..],
+                    level - 1,
+                    false,
+                    numpad,
+                    arrows,
+                    cache,
+                )
+            })
+            .min()
+            .or(Some(0))
+            .unwrap();
+        cache.insert((*char1, *char2, level), min as u64);
+        result += min;
     }
 
     result
