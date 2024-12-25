062c4e03097168fcde36fc4566ad96b13052e4af
ChristopherSoliman <csoliman10@gmail.com>
2024-12-25 17:18:19 +0200

    day 25: simplified parsing

--- src/days/day25/part1.rs
+++ src/days/day25/part1.rs
@@ -1,4 +1,4 @@
-pub fn part1(path: &str) -> u64 {
+pub fn part1(path: &str) -> u32 {
     let input = std::fs::read_to_string(path).expect("File should be there");
 
     let mut keys: Vec<[u8; 5]> = Vec::new();
@@ -7,22 +7,15 @@ pub fn part1(path: &str) -> u64 {
     let mut lock;
     let mut heights;
     for schem in input.trim().split("\r\n\r\n").into_iter() {
-        heights = [6; 5];
+        heights = [0; 5];
         lock = false;
         if schem.lines().next().unwrap() == "#####" {
             lock = true;
-            heights = [0; 5];
         }
         for line in schem.lines() {
             line.chars().enumerate().for_each(|(i, char)| {
-                if lock {
-                    if char == '#' {
-                        heights[i] += 1;
-                    }
-                } else {
-                    if char != '#' {
-                        heights[i] -= 1;
-                    }
+                if char == '#' {
+                    heights[i] += 1;
                 }
             });
         }
@@ -33,13 +26,12 @@ pub fn part1(path: &str) -> u64 {
         }
     }
 
-    let mut sum = 0;
-    for lock in &locks {
-        for key in &keys {
-            if key.iter().zip(lock).all(|(k, l)| (k + l) < 7) {
-                sum += 1;
-            }
-        }
-    }
-    sum
+    keys.iter()
+        .map(|k| {
+            locks
+                .iter()
+                .filter(|l| k.iter().zip(*l).all(|(k, l)| k + l < 8))
+                .count() as u32
+        })
+        .sum::<u32>()
 }
